7796393678aa4271d75656d62aecf1535d575567
loam <soeren@openio.io>
2017-04-29 12:11:02 +0900

    Add duration and pause icon

--- extension/main.js
+++ extension/main.js
@@ -2,19 +2,30 @@
     const TARGET = 'http://127.0.0.1:12346'
 
     setInterval(() => {
-        chrome.tabs.query({url: 'https://www.youtube.com/*'}, (tabs) => {
+        chrome.tabs.query({url: 'https://www.youtube.com/watch*'}, (tabs) => {
             let xhr = new XMLHttpRequest();
 
             for (let tab of tabs) {
                 if (tab.audible) {
-                    xhr.open('GET', `${TARGET}/${encodeURIComponent(tab.title)}`);
-                    xhr.send();
+                    chrome.tabs.executeScript(tab.id, {code: '(' + (() => {
+                        let video = document.getElementsByTagName('video');
+
+                        if (!video.length) {
+                            return [0, 0];
+                        }
+                        return [video[0].currentTime || 0, video[0].duration || 0];
+                    }) + ')();'}, ([[current, duration]]) => {
+                        xhr.open('GET', `${TARGET}/play/${encodeURIComponent(tab.title)}/${current}/${duration}`);
+                        xhr.send();
+                    });
 
                     return;
                 }
             }
 
-            xhr.open('GET', `${TARGET}/`);
+            let path = tabs.map((tab) => encodeURIComponent(tab.title)).join('/');
+
+            xhr.open('GET', `${TARGET}/pause/${path}`);
             xhr.send();
         })
     }, 1000);
--- indicator/main.go
+++ indicator/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/conformal/gotk3/gtk"
 	"github.com/doxxan/appindicator"
@@ -11,25 +15,87 @@ import (
 )
 
 var (
-	icon      string
-	indicator *gotk3.AppIndicatorGotk3
+	play, pause, idle string
+	playing           string
+	indicator         *gotk3.AppIndicatorGotk3
 )
 
 func init() {
-	flag.StringVar(&icon, "icon", "", "Path to the icon")
+	flag.StringVar(&play, "play", "", "Path to the play icon")
+	flag.StringVar(&pause, "pause", "", "Path to the pause icon")
+	flag.StringVar(&idle, "idle", "∅∅∅∅∅∅∅∅∅∅", "Idle message")
 
 	flag.Parse()
 }
 
 func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/" {
-		indicator.SetLabel("∅", "")
-
+	if indicator == nil {
 		return
 	}
 
-	title := req.URL.Path[1 : len(req.URL.Path)-10]
-	indicator.SetLabel(title, "")
+	path := decodePath(req.URL)
+
+	switch path[0] {
+	case "play":
+		title := cleanTitle(path[1])
+		playing = title
+
+		current := formatDuration(path[2])
+		duration := formatDuration(path[3])
+
+		indicator.SetIcon(play, "")
+		indicator.SetLabel(fmt.Sprintf("%s (%s / %s)", title, current, duration), "")
+	case "pause":
+		indicator.SetIcon(pause, "")
+
+		for i := 1; i < len(path); i++ {
+			title := cleanTitle(path[i])
+			if title == playing {
+				return
+			}
+		}
+
+		indicator.SetLabel(idle, "")
+	default:
+	}
+}
+
+func decodePath(u *url.URL) []string {
+	if u.RawPath == "" {
+		return strings.Split(u.Path[1:], "/")
+	}
+
+	encodedPath := strings.Split(u.RawPath[1:], "/")
+
+	decodedPath := make([]string, len(encodedPath))
+	for i, encodedComponent := range encodedPath {
+		decodedComponent, _ := url.PathUnescape(encodedComponent)
+		decodedPath[i] = decodedComponent
+	}
+
+	return decodedPath
+}
+
+func cleanTitle(title string) string {
+	if len(title) < 10 {
+		return ""
+	}
+
+	return title[:len(title)-10]
+}
+
+func formatDuration(seconds string) string {
+	d, _ := time.ParseDuration(seconds + "s")
+
+	h := int(d / time.Hour)
+	m := int(d/time.Minute) % 60
+	s := int(d/time.Second) % 60
+
+	if h == 0 {
+		return fmt.Sprintf("%v:%02v", m, s)
+	}
+
+	return fmt.Sprintf("%v:%v:%02v", h, m, s)
 }
 
 func serve() {
@@ -43,10 +109,10 @@ func serve() {
 func indicate() {
 	gtk.Init(nil)
 
-	indicator = gotk3.NewAppIndicator("indicator-youtube", icon, appindicator.CategorySystemServices)
+	indicator = gotk3.NewAppIndicator("indicator-youtube", pause, appindicator.CategorySystemServices)
 
 	indicator.SetStatus(appindicator.StatusActive)
-	indicator.SetLabel("∅", "")
+	indicator.SetLabel(idle, "")
 
 	menu, err := gtk.MenuNew()
 	if err != nil {
